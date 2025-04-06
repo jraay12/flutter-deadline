d4c4554ca9fc66b12fe0787af72b10b2a889b7c5
John Ray <johnraycaete@gmail.com>
2025-04-06 20:47:44 +0800

    test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -30,7 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-         multiDexEnabled = true
+        multiDexEnabled = true
     }
 
     buildTypes {
